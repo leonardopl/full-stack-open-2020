e9a8c6e881a5ea27d05d8c760e0c749c7a625f93
Leonardo <leonardopl@users.noreply.github.com>
2021-03-03 08:17:50 +0100

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,19 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
 
     if (!persons.some(person => person.name === newName))
     {
-      setPersons(persons.concat( {name: newName} ))
+      setPersons(persons.concat( {name: newName, number: newNumber} ))
       setNewName('')
+      setNewNumber('')
     }
     else {
       alert(`${newName} is already added to phonebook`)
@@ -21,6 +23,10 @@ const App = () => {
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -34,15 +40,21 @@ const App = () => {
                 />
         </div>
         <div>
+          number: <input
+                  value={newNumber}
+                  onChange={handleNumberChange}
+                />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
-      </form>
+      </form> 
       <h2>Numbers</h2>
-        <ul>
+        <>
           {persons.map(person => 
-            <li key={person.name}> {person.name} </li>
+            <div key={person.name}>{person.name} {person.number}</div>
           )}
-        </ul>
+        </>
     </div>
   )
 }
